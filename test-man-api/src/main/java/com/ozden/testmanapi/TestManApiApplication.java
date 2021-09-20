a7f61311a693ef0aa6314b420c5ba120f9ff5029
thsn-pg <thsn-pg>
2021-09-20 18:33:31 +0200

    initial commit

---
+++ test-man-api/src/main/java/com/ozden/testmanapi/TestManApiApplication.java
@@ -0,0 +1,13 @@
+package com.ozden.testmanapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestManApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestManApiApplication.class, args);
+    }
+
+}
---
+++ test-man-api/src/test/java/com/ozden/testmanapi/TestManApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ozden.testmanapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestManApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
