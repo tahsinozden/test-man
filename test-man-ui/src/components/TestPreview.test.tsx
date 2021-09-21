079b25d7de4526490488ae7de6d72243923e1c3a
thsn-pg <thsn-pg>
2021-09-21 23:10:25 +0200

    add test preview tests

---
+++ test-man-ui/src/components/TestPreview.test.tsx
@@ -0,0 +1,30 @@
+import renderer from "react-test-renderer";
+import TestPreview from "./TestPreview";
+import TestDetail from "../models/TestDetail";
+
+it('renders correctly when there is a test detail', () => {
+    const testDetail = new TestDetail(1, "name", "Passed", "");
+    const tree = renderer
+        .create(<TestPreview onStatusSelect={() => {
+        }} testDetail={testDetail}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there is a test detail with undefined props', () => {
+    const testDetail = new TestDetail();
+    const tree = renderer
+        .create(<TestPreview onStatusSelect={() => {
+        }} testDetail={testDetail}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there is no status present', () => {
+    const testDetail = new TestDetail(1, "name", undefined, "");
+    const tree = renderer
+        .create(<TestPreview onStatusSelect={() => {
+        }} testDetail={testDetail}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
---
+++ test-man-ui/src/components/TestPreviewList.test.tsx
@@ -0,0 +1,23 @@
+import renderer from "react-test-renderer";
+import TestPreviewList from "./TestPreviewList";
+import TestDetail from "../models/TestDetail";
+
+it('renders correctly when there are no tests', () => {
+    const tree = renderer
+        .create(<TestPreviewList onTestDataChange={() => {
+        }} testDetails={[]}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there are some tests', () => {
+    const testDetails = [
+        new TestDetail(1, "name1", "Passed", ""),
+        new TestDetail(2, "name2", "Failed", ""),
+    ];
+    const tree = renderer
+        .create(<TestPreviewList onTestDataChange={() => {
+        }} testDetails={testDetails}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
