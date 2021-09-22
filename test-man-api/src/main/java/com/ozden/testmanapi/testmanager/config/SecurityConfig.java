0161291579fbffda72ca45ae1ce0a02029a41acc
thsn-pg <thsn-pg>
2021-09-22 08:39:08 +0200

    update config

--- test-man-api/src/main/java/com/ozden/testmanapi/testmanager/config/SecurityConfig.java
+++ test-man-api/src/main/java/com/ozden/testmanapi/testmanager/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins(allowedOrigin)
                         .allowedMethods("HEAD", "OPTIONS", "GET", "POST", "PUT", "PATCH", "DELETE");
             }
         };
