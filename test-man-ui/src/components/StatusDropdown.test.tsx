ea501444ae6bc44634fa8cfe2c27c07b85029cc8
tahsinozden <tahsinozden>
2021-09-24 15:49:50 +0200

    add more test cases for status dropdown

--- test-man-ui/src/components/StatusDropdown.test.tsx
+++ test-man-ui/src/components/StatusDropdown.test.tsx
@@ -19,3 +19,30 @@ it('renders correctly when there is a empty status text', () => {
         .toJSON();
     expect(tree).toMatchSnapshot();
 });
+
+it('renders correctly when there is a current status "Passed"', () => {
+    const tree = renderer
+        .create(<StatusDropdown currentStatus={"Passed"}
+                                onStatusSelect={() => {
+                                }}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there is a current status "Failed"', () => {
+    const tree = renderer
+        .create(<StatusDropdown currentStatus={"Failed"}
+                                onStatusSelect={() => {
+                                }}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
+
+it('renders correctly when there is a current status "Undefined"', () => {
+    const tree = renderer
+        .create(<StatusDropdown currentStatus={"Undefined"}
+                                onStatusSelect={() => {
+                                }}/>)
+        .toJSON();
+    expect(tree).toMatchSnapshot();
+});
