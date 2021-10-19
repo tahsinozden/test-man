0d9d41b8c8c25d1aaf334cd4b06c89e80aab7273
tahsin <tahsin>
2021-10-19 22:05:07 +0200

    add preview test and update snapshots

--- test-man-ui/src/components/TestPreviewList.test.tsx
+++ test-man-ui/src/components/TestPreviewList.test.tsx
@@ -1,24 +1,46 @@
 import renderer from "react-test-renderer";
 import TestPreviewList from "./TestPreviewList";
 import TestDetail from "../models/TestDetail";
+import {render, screen} from "@testing-library/react";
 import "../jestGlobalMocks"
 
-it('renders correctly when there are no tests', () => {
-    const tree = renderer
-        .create(<TestPreviewList onTestDataChange={() => {
-        }} testDetails={[]}/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+describe("TestPreviewList component", () => {
+    it('renders correctly when there are no tests', () => {
+        const tree = renderer
+            .create(<TestPreviewList onTestDataChange={() => {
+            }} testDetails={[]}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
 
-it('renders correctly when there are some tests', () => {
-    const testDetails = [
-        new TestDetail(1, "name1", "Passed", ""),
-        new TestDetail(2, "name2", "Failed", ""),
-    ];
-    const tree = renderer
-        .create(<TestPreviewList onTestDataChange={() => {
-        }} testDetails={testDetails}/>)
-        .toJSON();
-    expect(tree).toMatchSnapshot();
-});
+    it('renders correctly when there are some tests', () => {
+        const testDetails = [
+            new TestDetail(1, "name1", "Passed", ""),
+            new TestDetail(2, "name2", "Failed", ""),
+        ];
+        const tree = renderer
+            .create(<TestPreviewList onTestDataChange={() => {
+            }} testDetails={testDetails}/>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('shows test list correctly when there are some tests', () => {
+        // given
+        const testDetails = [
+            new TestDetail(1, "name1", "Passed", ""),
+            new TestDetail(2, "name2", "Failed", ""),
+        ];
+
+        // when
+        render(<TestPreviewList onTestDataChange={() => {
+        }} testDetails={testDetails}/>);
+
+        // then
+        let listItem = screen.getByText("name1");
+        expect(listItem).toBeInTheDocument();
+
+        listItem = screen.getByText("name2");
+        expect(listItem).toBeInTheDocument();
+    });
+})
